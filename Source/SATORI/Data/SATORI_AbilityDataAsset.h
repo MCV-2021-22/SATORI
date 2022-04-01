58cd3be1c08909a0c9c7d195e5471cae48135a10
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-04-01 18:49:23 +0100

    change project path

---
+++ Source/SATORI/Data/SATORI_AbilityDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SATORI_AbilityDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SATORI_API USATORI_AbilityDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
