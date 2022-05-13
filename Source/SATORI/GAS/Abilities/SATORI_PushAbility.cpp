02abfb73d63060d221d01f8cc8a238757e4ca7ad
Nacho <87343965+eneaceo@users.noreply.github.com>
2022-05-13 11:30:40 +0200

    Added Push Ability
    Added Push ability
    Added two new Object types of collision (IgnoreSelfOverlapsAll, BlockOnlyStatic)
    Added two new Tags (Player, Enemy)

---
+++ Source/SATORI/GAS/Abilities/SATORI_PushAbility.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Abilities/SATORI_PushAbility.h"
+#include "AbilitySystemComponent.h"
+#include "SATORICharacter.h"
+
+USATORI_PushAbility::USATORI_PushAbility ()
+{
+
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+	//Ability Tags
+	FGameplayTag PushTag = FGameplayTag::RequestGameplayTag(FName("Ability.Push"));
+	AbilityTags.AddTag(PushTag);
+	ActivationOwnedTags.AddTag(PushTag);
+
+	BlockAbilitiesWithTag.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability")));
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability")));
+
+	//Ability default parameters
+	CastDelay = 0.2f;
+	Speed = 2000.0f;
+	Range = 1000.0f;
+	AngleRange = 30.0f;
+	SphereRadius = 32.0f;
+
+}
+
+void USATORI_PushAbility::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle, 
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo, 
+	const FGameplayEventData* TriggerEventData)
+{
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+	
+	ASATORICharacter* Character = Cast<ASATORICharacter>(GetAvatarActorFromActorInfo());
+
+	if (!Character)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+
+	//This calcs are for designing parameters for the ability
+	// 
+	//Calc for number of spheres to spawn to cover all range
+	int NumberOfSpheresToSpawn = (FMath::Tan(FMath::DegreesToRadians(AngleRange))) * Range * 2 / (SphereRadius * 2);
+	UE_LOG(LogTemp, Display, TEXT("Number of spheres: %d"), NumberOfSpheresToSpawn);
+
+	//Calc for cone spawning
+	FRotator RotationOfSphere = Character->GetActorRotation();
+	RotationOfSphere.Yaw -= AngleRange / 2;
+	float IncrementAngle = AngleRange / NumberOfSpheresToSpawn;
+	
+	float TimeToDestroy = Range / Speed; //Time to destroy sphere
+
+	//Spawn location
+	FVector InFront = Character->GetActorForwardVector() * 100.0f;
+	FTransform SphereTransform = Character->GetTransform();
+	SphereTransform.AddToTranslation(InFront);
+
+	for (int i = 0; i < NumberOfSpheresToSpawn; i++) {
+
+		//Spawn Rotation
+		RotationOfSphere.Yaw += IncrementAngle;
+		SphereTransform.SetRotation(RotationOfSphere.Quaternion());
+		
+		ASATORI_PushSphere* Sphere = GetWorld()->SpawnActor<ASATORI_PushSphere>(
+			PushSphereClass, //Actor to Spawn
+			Character->GetActorLocation() + Character->GetActorForwardVector() * 100, //Spawn location
+			RotationOfSphere); //Spawn rotation
+		//if(Sphere)
+		//	Sphere->InitializeParameters(SphereRadius, Speed, TimeToDestroy);
+	}
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+
+}
+
+void USATORI_PushAbility::OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+void USATORI_PushAbility::OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void USATORI_PushAbility::EventReceived(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+
+}
---
+++ Source/SATORI/GAS/Abilities/SATORI_PushAbility.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/SATORI_GameplayAbility.h"
+#include "SATORI/AbilityTask/SATORI_PlayMontageandWaitNotify.h"
+#include "Actors/PushSphere/SATORI_PushSphere.h"
+#include "SATORI_PushAbility.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class SATORI_API USATORI_PushAbility : public USATORI_GameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	USATORI_PushAbility();
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UAnimMontage* PushAnimMontage;
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		const FGameplayEventData* TriggerEventData) 
+		override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<ASATORI_PushSphere> PushSphereClass;
+
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	float CastDelay;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (UIMin = "50.0"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (UIMin = "100.0", UIMax = "10000.0"))
+	float Range;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (UIMin = "5.0", UIMax = "85.0"))
+	float AngleRange;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (UIMin = "8.0", UIMax = "128.0"))
+	float SphereRadius;
+
+	UFUNCTION()
+	void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void EventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
+	
+};
