6344e6dc4744d19a5fa249b94c98791f335e17f5
Nacho <87343965+eneaceo@users.noreply.github.com>
2022-05-13 11:48:16 +0200

    Fixed some conflicting files

--- Source/SATORI/GAS/Abilities/SATORI_TestAbility.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GAS/Abilities/SATORI_TestAbility.h"
-
-USATORI_TestAbility::USATORI_TestAbility() {
-
-}
