2457d08a39bb60213f172b450add1657a4d404f9
Alejandro Muñoz de León <alejandro.munozdeleon@alum.upf.edu>
2022-05-23 18:58:07 +0200

    solution

--- Source/SATORI/GAS/Abilities/SATORI_ChooseAbility.h
+++ Source/SATORI/GAS/Abilities/SATORI_ChooseAbility.h
@@ -3,23 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-<<<<<<<< HEAD:Source/SATORI/GAS/Abilities/SATORI_TimeSlowAbility.h
-#include "GAS/SATORI_GameplayAbility.h"
-#include "SATORI_TimeSlowAbility.generated.h"
-========
 #include "Abilities/GameplayAbility.h"
 #include "SATORI_ChooseAbility.generated.h"
->>>>>>>> Alejandro:Source/SATORI/GAS/Abilities/SATORI_ChooseAbility.h
 
 /**
  * 
  */
 UCLASS()
-<<<<<<<< HEAD:Source/SATORI/GAS/Abilities/SATORI_TimeSlowAbility.h
-class SATORI_API USATORI_TimeSlowAbility : public USATORI_GameplayAbility
-========
 class SATORI_API USATORI_ChooseAbility : public UGameplayAbility
->>>>>>>> Alejandro:Source/SATORI/GAS/Abilities/SATORI_ChooseAbility.h
 {
 	GENERATED_BODY()
 
