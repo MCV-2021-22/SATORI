3329cf1cb15e045edf28be1c9fa9ddd78e82c8b3
Alejandro Muñoz de León <alejandro.munozdeleon@alum.upf.edu>
2022-05-26 19:58:25 +0200

    Small Changes

--- Source/SATORI/AI/Character/Melee/SATORI_Melee.cpp
+++ Source/SATORI/AI/Character/Melee/SATORI_Melee.cpp
@@ -3,3 +3,10 @@
 
 #include "AI/Character/Melee/SATORI_Melee.h"
 
+ASATORI_Melee::ASATORI_Melee()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bte = TSoftObjectPtr <UBehaviorTree>(FSoftObjectPath(TEXT("/Game/SATORI/AI/Melee/BT_MELEE.BT_MELEE")));
+
+}
