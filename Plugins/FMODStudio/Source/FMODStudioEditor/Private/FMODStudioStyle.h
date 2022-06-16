ec46d31bd1c0efcf13c68e4ba7f1ecb9df94c7a0
AleeMD44 <99.alejandro.md@gmail.com>
2022-06-16 22:39:58 +0200

    Plugin added

---
+++ Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioStyle.h
@@ -0,0 +1,10 @@
+// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2022.
+
+#pragma once
+#include "EditorStyle/Public/EditorStyleSet.h"
+
+class FFMODStudioStyle : public FEditorStyle
+{
+public:
+    static void Initialize();
+};
