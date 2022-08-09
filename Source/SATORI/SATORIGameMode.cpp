0b8040d49cbd1eced6cb4104d2c54a52ec2b35e8
Nacho <87343965+eneaceo@users.noreply.github.com>
2022-08-10 01:08:54 +0200

    Update SATORIGameMode.cpp

--- Source/SATORI/SATORIGameMode.cpp
+++ Source/SATORI/SATORIGameMode.cpp
@@ -10,10 +10,10 @@
 ASATORIGameMode::ASATORIGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SATORI/Character/SATORI_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SATORI/Character/SATORI_Character_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("/Game/SATORI/SATORI_Character"));
+		UE_LOG(LogTemp, Warning, TEXT("/Game/SATORI/SATORI_Character_C"));
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
