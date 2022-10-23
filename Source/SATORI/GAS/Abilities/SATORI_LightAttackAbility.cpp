8e4d01f65848485cda1df5b71d2588787a7c84f5
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-10-23 15:07:50 +0100

    Cancel Ability 2 Iteration

---
+++ Source/SATORI/GAS/Abilities/SATORI_LightAttackAbility.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Abilities/SATORI_LightAttackAbility.h"
+#include "AbilitySystemComponent.h"
+#include "SATORICharacter.h"
+#include "Character/SATORI_PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "GAS/SATORI_AbilitySystemComponent.h"
+#include "Components/Player/SATORI_ComboSystemComponent.h"
+#include "SATORI/AbilityTask/SATORI_PlayMontageAndWaitEvent.h"
+
+void USATORI_LightAttackAbility::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!IsValid(AnimMontage))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] USATORI_LightAttackAbility: Cannot get Animation Montage ... "), *GetName());
+		Super::EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+
+	PlayerCharacter = Cast<ASATORICharacter>(GetAvatarActorFromActorInfo());
+	if (!PlayerCharacter)
+	{
+		UE_LOG(LogTemp, Display, TEXT("[%s] USATORI_DashAbiUSATORI_LightAttackAbilitylity: Cannot Cast ASATORICharacter ... "), *GetName());
+		Super::EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		UE_LOG(LogTemp, Display, TEXT("[%s] USATORI_LightAttackAbility: Failed commit ability ... "), *GetName());
+		Super::EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+
+	USATORI_PlayMontageAndWaitEvent* Task = USATORI_PlayMontageAndWaitEvent::PlayMontageAndWaitForEvent(this, NAME_None, AnimMontage, FGameplayTagContainer(), 1.0f, NAME_None, bStopWhenAbilityEnds, 1.0f);
+	Task->OnBlendOut.AddDynamic(this, &USATORI_LightAttackAbility::OnCompleted);
+	Task->OnCompleted.AddDynamic(this, &USATORI_LightAttackAbility::OnCompleted);
+	Task->OnInterrupted.AddDynamic(this, &USATORI_LightAttackAbility::OnCancelled);
+	Task->OnCancelled.AddDynamic(this, &USATORI_LightAttackAbility::OnCancelled);
+	Task->EventReceived.AddDynamic(this, &USATORI_LightAttackAbility::EventReceived);
+	Task->ReadyForActivation();
+
+}
+
+void USATORI_LightAttackAbility::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void USATORI_LightAttackAbility::CancelAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateCancelAbility)
+{
+
+
+	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+}
+
+void USATORI_LightAttackAbility::OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void USATORI_LightAttackAbility::OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void USATORI_LightAttackAbility::EventReceived(FGameplayTag EventTag, FGameplayEventData EventData)
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
---
+++ Source/SATORI/GAS/Abilities/SATORI_LightAttackAbility.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/SATORI_GameplayAbility.h"
+#include "SATORI_LightAttackAbility.generated.h"
+
+class ASATORICharacter;
+
+UCLASS()
+class SATORI_API USATORI_LightAttackAbility : public USATORI_GameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Ability")
+	UAnimMontage* AnimMontage;
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData)
+		override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled) override;
+
+	virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateCancelAbility) override;
+
+public:
+
+	bool bStopWhenAbilityEnds = true;
+
+protected:
+	UFUNCTION()
+	void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void EventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	ASATORICharacter* PlayerCharacter = nullptr;
+
+};
