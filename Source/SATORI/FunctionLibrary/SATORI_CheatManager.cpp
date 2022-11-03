12b232e01076f036c12bc01315625a817be1daf6
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-11-03 19:30:19 +0000

    cheat ui

--- Source/SATORI/FunctionLibrary/SATORI_CheatManager.cpp
+++ Source/SATORI/FunctionLibrary/SATORI_CheatManager.cpp
@@ -6,6 +6,7 @@
 #include "Components/Player/SATORI_GameplayAbilityComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Character/SATORI_CharacterBase.h"
+#include "Components/Player/SATORI_ComboSystemComponent.h"
 
 void USATORI_CheatManager::UnlockAllAbilities(bool Value)
 {
@@ -35,5 +36,18 @@ void USATORI_CheatManager::IncreasedAttack()
 	if (Character)
 	{
 		Character->WeaponDamage = 1000.0f;
+	}
+}
+
+void USATORI_CheatManager::ShowMainUI()
+{
+	ASATORICharacter* Character = Cast<ASATORICharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if (Character)
+	{
+		USATORI_ComboSystemComponent* ComboComp = Character->GetComboSystemComponent();
+		if (ComboComp)
+		{
+			ComboComp->SetMainWidgetVisibility(true);
+		}
 	}
 }
